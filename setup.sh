dd8ad33994cdc18359c0a1b0115ff0471c0ff724
lorenuar <lorenuar.s19@gmail.com>
2020-07-16 00:55:23 +0200

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,6 @@
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
+echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >>! ~/.zshrc
+
+zsh -c "$(p10k configure)"
