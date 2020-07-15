ea6c72b228e538026d4d1bd3560d230296e509dc
lorenuar <lorenuar.s19@gmail.com>
2020-07-16 01:05:02 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,9 @@
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+printf "= = = SETUP = = =\n"
 
+printf "+ + + OMZSH + + +\n"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh --unattended)"
+
+printf "+ + + P10K + + +\n"
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
 echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >> ~/.zshrc
 source ~/.zshrc
