b7aca31f0ca4044cdb5cecd1aeccec5b60cd7a99
lorenuar <lorenuar.s19@gmail.com>
2020-07-16 00:59:46 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,5 +2,6 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/too
 
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
 echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >>! ~/.zshrc
+source ~/.zshrc
 
 zsh -c "$(p10k configure)"
