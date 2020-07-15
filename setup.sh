ff1bd09b48125d531d415b68e31127228d591603
lorenuar <lorenuar.s19@gmail.com>
2020-07-16 01:24:57 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,13 +1,21 @@
-printf "= = = SETUP = = =\n"
+printf "\033[32m= = = SETUP = = =\n"
 cd ~
-printf "+ + + OMZSH + + +\n"
+printf "\033[32m+ + + OMZSH + + +\n"
 zsh
 curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh OH_MY_ZSH-install.sh
 ./OH_MY_ZSH-install.sh --unattended
 
-printf "+ + + P10K + + +\n"
+printf "\033[32m+ + + P10K + + +\n"
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
-echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >> ~/.zshrc
+
+curl -fsSL https://raw.githubusercontent.com/lorenuars19/Guac_quick_config/master/.zshrc ~/.zshrc
+curl -fsSL https://raw.githubusercontent.com/lorenuars19/Guac_quick_config/master/.p10k.zsh ~/.p10k.zsh
+
 source ~/.zshrc
 
-zsh -c "$(p10k configure)"
+printf "\033[32m+ + + SSH + + +\n"
+ssh-keygen
+
+echo "\n\n"
+cat ~/.ssh/id_rsa.pub
+
