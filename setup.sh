9763bc4f66207e03209dceb70535a03606a776d8
lorenuar <lorenuar.s19@gmail.com>
2020-07-21 16:52:18 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -13,8 +13,8 @@ mv .p10k.zsh .old.p10k.zsh
 
 printf "\033[34mV V V DOWNLOAD DOT FILES V V V\n\033[m"
 
-sh -c "curl https://raw.githubusercontent.com/lorenuars19/Guac_quick_config/master/zshrc" > ~/.zshrc
-sh -c "curl https://raw.githubusercontent.com/lorenuars19/Guac_quick_config/master/p10k-zsh" > ~/.p10k.zsh
+curl  https://raw.githubusercontent.com/lorenuars19/Guac_quick_config/master/zshrc -o ~/.zshrc
+curl  https://raw.githubusercontent.com/lorenuars19/Guac_quick_config/master/p10k-zsh -o ~/.p10k.zsh
 
 printf "\033[34mV V V SOURCE V V V\n\033[m"
 source ~/.zshrc
