43f2a131c5b9b6555d6fa7352dbb46442f43e8b4
Louis Renuart <lorenuar@shi-r1-p5.s19.be>
2020-08-18 20:19:52 +0200

    added vimrc to script

--- setup.sh
+++ setup.sh
@@ -18,7 +18,9 @@ curl -s https://raw.githubusercontent.com/lorenuars19/Guac_quick_config/master/z
 printf "\033[32m+ + + .zshrc \033[m\n"
 curl -s https://raw.githubusercontent.com/lorenuars19/Guac_quick_config/master/p10k-zsh -o ~/.p10k.zsh /dev/null
 printf "\033[32m+ + + .p10k.zsh \033[m\n"
-
+curl -s https://raw.githubusercontent.com/lorenuars19/Guac_quick_config/master/vimrc -o ~/.vimrc > /dev/null
+printf "\033[32m+ + + .vimrc \033[m\n"
+c
 printf "\033[34mV V V SOURCE V V V\n\033[m"
 source ~/.zshrc > /dev/null 2>&1
 
