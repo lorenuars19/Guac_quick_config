7d0bb6b240624325a9c39b6b388e0de91a54ce6d
lorenuar <lorenuar.s19@gmail.com>
2020-09-21 11:35:51 +0200

    added brew

--- setup.sh
+++ setup.sh
@@ -35,3 +35,6 @@ cat ~/.ssh/id_rsa.pub
 
 printf "\n\n\033[m"
 
+
+rm -rf $HOME/.brew && git clone --depth=1 https://github.com/Homebrew/brew $HOME/.brew && echo 'export PATH=$HOME/.brew/bin:$PATH' >> $HOME/.zshrc && source $HOME/.zshrc && brew update
+
